97773ae794d1de866b81b39aa0ae043693212c1a
Matt Silbernagel <silbermm@gmail.com>
2016-12-15 19:51:47 -0500

    Show webhooks on page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,10 +1,7 @@
 defmodule RallyHookProxy.PageController do
   use RallyHookProxy.Web, :controller
-  require IEx
 
   def index(conn, _params) do
-    IEx.pry
-    user = Guardian.Plug.current_resource(conn)
     render conn, "index.html"
   end
 end
